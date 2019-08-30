f6aac33fa11281470cfe899aa047eea27023f253
Yurii Husak <gusak.1881@gmail.com>
2019-08-30 23:42:00 +0300

    layout refactoring

--- src/components/Controls.vue
+++ src/components/Controls.vue
@@ -2,26 +2,16 @@
     <div class="container-fluid">
         <div>
             <h5>Background sizes</h5>
-            <div class="pane visible">
-                <div class="row">
-                    <label for="width">Width</label>
-                    <input type="number" id="width" step="1" min="0" class="form-control">
-                </div>
-                <div class="row">
-                    <label for="height">Height</label>
-                    <input type="number" id="height" step="1" min="0" class="form-control">
-                </div>
-                <div class="row">
-                    <label for="radius">Radius</label>
-                    <input type="number" id="radius" step="1" min="0" class="form-control">
-                </div>
-                <div class="row">
-                    <label for="color">Color</label>
-                    <input type="color" id="color" class="form-control">
-                </div>
-                <div class="row">
-                    <label for="alpha">Alpha</label>
-                    <input type="number" id="alpha" min="0" max="1" class="form-control">
+            <div v-for="(param, name) in params">
+                <div class="row input-group-sm mb-1">
+                    <label v-bind:for="name" class="m-0">{{name}}</label>
+                    <input v-bind:id="name"
+                           v-bind:value="param"
+                           v-bind:type="(name === 'color' ? 'color' : 'number')"
+                           v-on:input="updateParam"
+                           class="form-control p-1"
+                           step="1"
+                           min="0">
                 </div>
             </div>
         </div>
@@ -29,10 +19,18 @@
 </template>
 
 <script>
-    //TODO 1) add the rest of the input fields
-    //TODO 2) Connect all fields to storage so they can be generated dynamically
+  import { mapGetters } from "vuex";
+
   export default {
-    name: "Controls"
+    name: "Controls",
+    computed: {
+      ...mapGetters(["button",  "params"])
+    },
+    methods: {
+      updateParam(e){
+        //TODO connect mutations of vuex with this component
+      }
+    }
   };
 </script>
 
