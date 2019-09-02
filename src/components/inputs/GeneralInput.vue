624982f9330f31b39bb5917c76ce4503240bf160
Yurii Husak <gusak.1881@gmail.com>
2019-09-03 00:54:59 +0300

    New sub-components were created and added

---
+++ src/components/inputs/GeneralInput.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="row input-group-sm mb-1">
+        <label v-bind:for="data.name" class="m-0">{{data.label || data.name}}</label>
+        <input v-bind:id="data.name"
+               v-bind:value="data.value"
+               v-bind:type="data.type"
+               v-bind:key="data.key"
+               v-on:input="onInput"
+               class="form-control p-1"
+               min="0">
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "TextInput",
+    params: ["data"],
+    methods: {
+      onInput({ target: { id, value } }) {
+
+      }
+    }
+  };
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/inputs/SelectInput.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+  export default {
+    name: "SelectInput"
+  };
+</script>
+
+<style scoped>
+
+</style>
