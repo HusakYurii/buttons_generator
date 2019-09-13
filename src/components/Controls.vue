0bb2f844df00e9a556c9c17eec5da426eb8401e9
Yurii Husak <gusak.1881@gmail.com>
2019-09-13 21:46:24 +0300

    Read me was updated

--- src/components/Controls.vue
+++ src/components/Controls.vue
@@ -2,17 +2,50 @@
     <div class="controls-container">
         <GraphicInputs/>
         <TextInputs/>
+        <button id="downloadConfig" v-on:click.prevent="downloadConfig">DOWNLOAD CONFIG</button>
     </div>
 </template>
 
 <script>
   import { GraphicInputs, TextInputs } from "./sub-components";
+  import {mapGetters} from "vuex";
 
   export default {
     name: "Controls",
     components: {
       GraphicInputs,
       TextInputs
+    },
+    methods: {
+      ...mapGetters(["graphicsOutputs", "textOutputs"]),
+      downloadConfig(){
+        const outputs = {
+          button: this.graphicsOutputs(),
+          text: this.textOutputs()
+        };
+
+        const config =  Object.entries(outputs).reduce((acc, [key, value]) => {
+          const { anchorX: x, anchorX: y, picture ,...rest } = value;
+          acc[key] = {
+            anchor: { x, y },
+            picture,
+            ...rest
+          };
+          return acc;
+        }, {});
+        this.download(config);
+      },
+      download(config){
+        const content = JSON.stringify(config, null, "\t");
+        const type = "data:application/json;charset=utf-8";
+        const file = new Blob([content], {type});
+        const fileName = "button.json";
+
+        const aTag = document.createElement("a");
+        aTag.href = URL.createObjectURL(file);
+        aTag.download = fileName;
+        aTag.click();
+      }
     }
   };
 </script>
